11dec088962e52d42d576409dfa07722d94900f1
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-11-15 19:52:06 +0200

    Add project files.

---
+++ Lesson2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Lesson2
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] color = new string[]
+            {
+        "Navy",
+        "Blue",
+        "Aqua",
+        "Teal",
+        "Olive",
+        "Green",
+        "Lime",
+        "Yellow",
+        "Orange",
+        "Red"
+            };
+
+            int i = 0;
+
+            foreach (Button button in wrapButtonsPanel.Children)
+            {
+                if (i < color.Length)
+                {
+                    button.Content = color[i];
+                    button.Background = (Brush)new BrushConverter().ConvertFromString(color[i]);
+                    i++;
+                }
+            }
+        }
+    }
+}
